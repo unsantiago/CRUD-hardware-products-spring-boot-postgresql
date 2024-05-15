2f94253dc73af1068823d0c8dde54b67e3cd2116
unsantiago <lprietoa2@gmail.com>
2024-05-14 19:08:00 -0500

    Inicialización del repositorio con la estructura básica

---
+++ src/main/java/com/unsantiago/crud/CrudBasicoConSpringBootYJavaApplication.java
@@ -0,0 +1,13 @@
+package com.unsantiago.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicoConSpringBootYJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBasicoConSpringBootYJavaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/unsantiago/crud/entity/model/Product.java
@@ -0,0 +1,4 @@
+package com.unsantiago.crud.entity.model;
+
+public class Product {
+}
